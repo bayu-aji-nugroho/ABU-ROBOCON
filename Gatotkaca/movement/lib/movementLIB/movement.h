0fa78e9cefbf0c8f923d19367f6027ab753461a9
bayu-aji-nugroho <bayuastudies@gmail.com>
2026-02-04 16:16:12 +0700

    menambah platform.io gatotkaca

---
+++ Gatotkaca/movement/lib/movementLIB/movement.h
@@ -0,0 +1,28 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+//menggabungkan movement
+
+#include <Arduino.h>
+class Encoder;
+class PID;
+
+class Movement{
+    private:
+        Encoder* encoder;
+        PID* pid;
+        int RPWM,LPWM;
+        float nilai_PWM_ke_roda;
+    public:
+        Movement(
+            float Kp, float Ki, float Kd, int chanelA, int ChanelB,
+              float ppr,int RPWM, int LPWM);
+        void update(float target);
+        void begin();
+        void resetPID();
+
+
+};
+
+
+#endif
